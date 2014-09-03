07a1805df6ea5861deb9ce90eaab41225c1c2da1
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-09-03 23:42:25 +0300

    Initial commit
    The problem is solved using C#

---
+++ 01.ExOh/ExOH.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class ExOH
+{
+    static bool ExOh(string exOh)
+    {
+        byte oSum = 0;
+        byte xSum = 0;
+        bool answer = false;
+
+        foreach (var symbol in exOh)
+        {
+            if (symbol == 'o')
+            {
+                oSum += 1;
+            }
+            else
+            {
+                xSum += 1;
+            }
+        }
+        if (oSum == xSum)
+        {
+            answer = true;
+            
+        }
+        
+
+        return answer;
+    }
+    static void Main()
+    {
+        string exOh = Console.ReadLine();
+
+        bool finalAnswer = ExOh(exOh);
+
+        Console.WriteLine(finalAnswer);
+        
+    }
+}
+
